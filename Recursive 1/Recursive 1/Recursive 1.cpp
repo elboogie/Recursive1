00144f1b99ea47e84694a82fc556bbf471d79c6b
elboogie <elboogie@yandex.ua>
2016-11-09 14:18:32 +0300

    first commit

---
+++ Recursive 1/Recursive 1/Recursive 1.cpp
@@ -0,0 +1,16 @@
+// Recursive 1.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+
+int main()
+{
+
+
+	char ch;
+	get(ch);
+    return 0;
+}
+
