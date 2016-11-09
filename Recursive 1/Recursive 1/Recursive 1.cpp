a8d5fb7cadc4b3691944fa67161d1e882ea30a69
elboogie <elboogie@yandex.ua>
2016-11-09 14:39:28 +0300

    first commit

--- Recursive 1/Recursive 1/Recursive 1.cpp
+++ Recursive 1/Recursive 1/Recursive 1.cpp
@@ -1,16 +1,24 @@
 // Recursive 1.cpp : Defines the entry point for the console application.
 //
 
+// Read numbers from user, then corresponds it to a alphabet and output all 
 #include "stdafx.h"
 #include <iostream>
+#include <string>
+#include <sstream>
+
 
 using std::cin;
 using std::cout;
 
+
+constexpr char _0 = '0';
+
 int main()
 {
-
-
+	std::string input;
+	std::getline(cin, input);
+	
 	char ch;
 	cin >> ch;
     return 0;
